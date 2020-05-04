c13a4e184e35b9e9f95471c3886a9c53e827f16a
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2020-05-04 20:59:06 +0300

    Fix error with the props

--- app/client/src/pages/popularpage/PopularPage.js
+++ app/client/src/pages/popularpage/PopularPage.js
@@ -1,21 +1,44 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import './PopularPage.css';
 
-const PopularPage = ({ location: { state: { image, name, description, sold } } }) => {
-  return (
-    <section className="popular">
-      <div className="popular__wrapper">
-        <h2 className="popular__title title">{name}</h2>
-        <img src={image} alt={name} className="popular__img" />
-        <p className="popular__description ">{description}</p>
-        <div className="popular__sold">
-          Sold: <span>{sold}</span>
+class PopularPage extends Component {
+  state = {
+    image: '',
+    name: '',
+    description: '',
+    sold: '',
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    const { location: { state: st } } = props;
+    if (st) {
+      return {
+        image: st.image,
+        name: st.name,
+        description: st.description,
+        sold: st.sold,
+      };
+    }
+    return null;
+  }
+
+  render() {
+    const { image, name, description, sold } = this.state;
+    return (
+      <section className="popular">
+        <div className="popular__wrapper">
+          <h2 className="popular__title title">{name}</h2>
+          <img src={image} alt={name} className="popular__img" />
+          <p className="popular__description ">{description}</p>
+          <div className="popular__sold">
+            Sold: <span>{sold}</span>
+          </div>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 PopularPage.propTypes = {
   image: PropTypes.string,
