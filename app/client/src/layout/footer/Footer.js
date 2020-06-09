c14abe29e27dd900c22fe7421a63c0f555a39434
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2020-06-09 08:38:29 +0300

    Change date calculation

--- app/client/src/layout/footer/Footer.js
+++ app/client/src/layout/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  return <footer className="footer">Copyright © {new Date().toISOString().split('-')[0]}</footer>;
+  return <footer className="footer">Copyright © {new Date().getFullYear()}</footer>;
 };
 
 export default Footer;
